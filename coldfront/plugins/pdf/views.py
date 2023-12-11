2ce8e85d3e7e0d005fa25daeadcd9791773c89c3
Matthew Kusz <Matt52@roadrunner.com>
2023-12-11 14:38:53 -0500

    Finish customizing pdf

--- coldfront/plugins/pdf/views.py
+++ coldfront/plugins/pdf/views.py
@@ -1,9 +1,13 @@
 import io
+from datetime import date
 
-from reportlab.platypus import SimpleDocTemplate, Paragraph, Spacer
+from reportlab.platypus import SimpleDocTemplate, Paragraph, Spacer, KeepTogether, Table, TableStyle
 from reportlab.platypus.flowables import HRFlowable
 from reportlab.lib.pagesizes import letter
-from reportlab.lib.styles import getSampleStyleSheet
+from reportlab.lib.styles import getSampleStyleSheet, ParagraphStyle
+from reportlab.lib.units import inch
+from reportlab.lib.enums import TA_CENTER
+from reportlab.lib.colors import black
 
 from django.http import FileResponse
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
@@ -15,6 +19,15 @@ from django.contrib.auth.models import User
 
 from coldfront.core.project.models import ProjectUser, ProjectUserRoleChoice
 from coldfront.core.resource.models import ResourceType
+from coldfront.core.utils.common import import_from_settings
+
+CENTER_BASE_URL = import_from_settings('CENTER_BASE_URL', '')
+
+
+def add_page_number(canvas, doc):
+    text = f'Page {canvas.getPageNumber()}'
+    canvas.drawCentredString(letter[0] / 2, letter[1] / 20, text)
+
 
 class UserReportPDFView(LoginRequiredMixin, UserPassesTestMixin, View):
     def test_func(self):
@@ -22,12 +35,22 @@ class UserReportPDFView(LoginRequiredMixin, UserPassesTestMixin, View):
         if user.is_superuser:
             return True
         
-    def create_paragraph(self, story, label, text, style):
-        story.append(
+    def create_paragraph(self, flowable_list, label, text, style):
+        flowable_list.append(
             Paragraph(f'<strong>{label}:</strong> {text}', style)
         )
-        story.append(Spacer(1, 2))
-        return story
+        flowable_list.append(Spacer(1, 2))
+        return flowable_list
+    
+    def create_table(self, flowable_list, data, col_widths):
+        flowable_list.append(Table(data, colWidths=col_widths))
+        return flowable_list
+    
+    def create_row(self, data, name, value, style, char_limit=1400):
+        if len(value) > char_limit:
+            value = value[:char_limit] + '...'
+        data.append((Paragraph(name, style), Paragraph(value, style)))
+        return data
     
     def get_full_name(self, user_obj):
         full_name = f'{user_obj.first_name} {user_obj.last_name}'
@@ -62,7 +85,6 @@ class UserReportPDFView(LoginRequiredMixin, UserPassesTestMixin, View):
                 allocations[resource_type_obj.name] = allocations_by_type
 
         return allocations
-
 
     def get(self, request):
         username = request.GET.get('user')
@@ -154,53 +176,104 @@ class UserReportPDFView(LoginRequiredMixin, UserPassesTestMixin, View):
         style_title = styles.get('Title')
         style_heading1 = styles.get('Heading1')
         style_heading2 = styles.get('Heading2')
+        style_centered_italic = ParagraphStyle(
+            name='Centered Italic',
+            parent=styles.get('Italic'),
+            alignment=TA_CENTER
+        )
+        hr = HRFlowable(width='100%', spaceBefore=5, spaceAfter=16, color=black)
 
         story = []
-
-        pdf_title = Paragraph(f'User Report - {viewed_user.first_name} {viewed_user.last_name}', style_title)
+        pdf_title = Paragraph(
+            f'User Report - {viewed_user.first_name} {viewed_user.last_name}', style_title
+        )
         story.append(pdf_title)
-        story.append(Spacer(1, 2))
+        todays_date = Paragraph(date.today().isoformat(), style_centered_italic)
+        story.append(todays_date)
+        story.append(hr)
         for project_user_obj in qs:
+            keep_together = []
             project_obj = project_user_obj.project
-            story.append(HRFlowable(width='100%', spaceBefore=5, spaceAfter=5))
-            project_title = Paragraph(f'Project: {project_obj.title}', style_heading1)
-            story.append(project_title)
+            project_url = f'{CENTER_BASE_URL.strip("/")}/project/{project_obj.pk}'
+            project_title = Paragraph(f'<link href="{project_url}">{project_obj.title}</link>', style_heading1)
+            keep_together.append(project_title)
 
-            project_information = Paragraph(f'Project Information', style_heading2)
-            story.append(project_information)
-            story.append(HRFlowable(width='50%', spaceBefore=5, spaceAfter=5, hAlign='LEFT'))
-            story = self.create_paragraph(story, 'Status', project_obj.status.name, style_normal)
-            story = self.create_paragraph(story, 'Description', project_obj.description, style_normal)
+            project_information = Paragraph(f'<u>Project Information</u>', style_heading2)
+            keep_together.append(project_information)
+            data=[]
+            data = self.create_row(
+                data, '<strong>Status:</strong>', project_obj.status.name, style_normal
+            )
+            data = self.create_row(
+                data, '<strong>Description:</strong>', project_obj.description, style_normal
+            )
             project_pi = f'{project_obj.pi.first_name} {project_obj.pi.last_name}'
-            story = self.create_paragraph(story, 'PI', project_pi, style_normal)
+            data = self.create_row(data, '<strong>PI:</strong>', project_pi, style_normal)
             project_requestor = f'{project_obj.requestor.first_name} {project_obj.requestor.last_name}'
             if project_pi != project_requestor:
-                story = self.create_paragraph(story, 'Requestor', project_requestor, style_normal)
-            story = self.create_paragraph(story, 'Created', project_obj.created.date(), style_normal)
-
-            project_user_roles = Paragraph(f'Project User Roles', style_heading2)
-            story.append(project_user_roles)
-            story.append(HRFlowable(width='50%', spaceBefore=5, spaceAfter=5, hAlign='LEFT'))
-            project_user_objs = project_obj.projectuser_set.filter(status__name="Active")
-            for role, project_users in self.get_project_users_by_role(project_user_objs).items():
-                story = self.create_paragraph(
-                    story, role + 's', ', '.join(project_users), style_normal
+                data = self.create_row(
+                    data, '<strong>Requestor:</strong>', project_requestor, style_normal
                 )
+            data = self.create_row(
+                data, '<strong>Created:</strong>', project_obj.created.date().isoformat(), style_normal
+            )
+            self.create_table(keep_together, data, (1.0 * inch, None))
+            table = keep_together[-1]
+            table.setStyle(TableStyle(
+                [
+                    ('VALIGN', (0, 0), (0, len(data)), 'TOP'),
+                    ('HALIGN', (0, 0), (1, len(data)), 'LEFT'),
+                ]
+            ))
+            story.append(KeepTogether(keep_together))
+            
+            keep_together = []
+            project_user_roles = Paragraph(f'<u>Project Users</u>', style_heading2)
+            keep_together.append(project_user_roles)
+            project_user_objs = project_obj.projectuser_set.filter(status__name="Active")
+            data=[]
+            for role, project_users in self.get_project_users_by_role(project_user_objs).items():
+                data = self.create_row(
+                    data, f'<strong>{role}s:</strong>', ', '.join(project_users), style_normal
+                )
+            self.create_table(keep_together, data, (1.0 * inch, None))
+            table = keep_together[-1]
+            table.setStyle(TableStyle(
+                [
+                    ('VALIGN', (0, 0), (0, len(data)), 'TOP'),
+                    ('HALIGN', (0, 0), (1, len(data)), 'LEFT'),
+                ]
+            ))
+            story.append(KeepTogether(keep_together))
 
-            project_allocations = Paragraph(f'Project Allocations', style_heading2)
-            story.append(project_allocations)
-            story.append(HRFlowable(width='50%', spaceBefore=5, spaceAfter=5, hAlign='LEFT'))
-            allocation_objs =  project_obj.allocation_set.filter(status__name__in=["Active", "Renewal Requested"])
+            keep_together = []
+            project_allocations = Paragraph(f'<u>Project Allocations</u>', style_heading2)
+            keep_together.append(project_allocations)
+            allocation_objs =  project_obj.allocation_set.filter(
+                status__name__in=["Active", "Renewal Requested"]
+            )
             allocation_objs_by_type = self.get_project_allocations_by_type(allocation_objs).items()
+            data = []
             if allocation_objs_by_type:
                 for type, allocations in allocation_objs_by_type:
-                    story = self.create_paragraph(
-                        story, type,', '.join(allocations), style_normal 
+                    data = self.create_row(
+                        data, f'<strong>{type}:</strong>', ', '.join(allocations), style_normal
                     )
+                keep_together = self.create_table(keep_together, data, (1.3 * inch, None))
+                table = keep_together[-1]
+                table.setStyle(TableStyle(
+                    [
+                        ('VALIGN', (0, 0), (0, len(data)), 'TOP'),
+                        ('HALIGN', (0, 0), (1, len(data)), 'LEFT'),
+                    ]
+                ))
             else:
-                story.append(Paragraph('None', style_normal))
+                keep_together.append(Paragraph('None', style_normal))
 
-        doc.build(story)
+            keep_together.append(hr)
+            story.append(KeepTogether(keep_together))
+
+        doc.build(story, onFirstPage=add_page_number, onLaterPages=add_page_number)
         buffer.seek(0)
 
-        return FileResponse(buffer, as_attachment=True, filename="user_report.pdf")
+        return FileResponse(buffer, as_attachment=True, filename='user_report.pdf')
